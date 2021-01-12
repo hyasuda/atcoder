c87454d33816d41f1ed7ad9677361a4b3f5043e8
hyasuda <hyasuda.phys@gmail.com>
2021-01-12 16:45:19 +0900

    update : ABC188

--- ABC/ABC188/test.cpp
+++ ABC/ABC188/test.cpp
@@ -6,22 +6,20 @@ using P = pair<int, int>;
 
 
 int main(){
+
     int n;
     cin >> n;
-    int n2 = 1<<n;
+    int n2 = 1 << n;
     vector<int> a(n2);
     rep(i, n2) cin >> a[i];
     map<int, int> mp;
     rep(i, n2) mp[a[i]] = i+1;
-    while(a.size() > 2){
-        vector<int> na;
-        for(int i = 0; i < a.size(); i += 2){
-            na.push_back(max(a[i], a[i+1]));
-        }
-        swap(a, na);
+    while( a.size() > 2 ){
+
     }
+
     int ans = min(a[0], a[1]);
     // cout << ans << endl;
     cout << mp[ans] << endl;
-    return 0;
+
 }
